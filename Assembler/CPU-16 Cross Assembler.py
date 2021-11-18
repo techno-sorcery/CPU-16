f31edc0233ef60a92d2ed05fbaa2a11fe9bd0479
Hayden B <inquisitivecalculator@gmail.com>
2021-11-18 10:33:45 -0800

    Update

--- Assembler/CPU-16 Cross Assembler.py
+++ Assembler/CPU-16 Cross Assembler.py
@@ -15,14 +15,25 @@ fileParse = fileParse.strip()
 lines = fileParse.splitlines()
 #print(lines)
 
+adsImm = re.compile(r'^#(?P<immediate>[0-9A-F]+)\Z')
+adsReg = re.compile(r'^d(?P<register>[0-7]+)\Z')
+adsDir = re.compile(r'^\$(?P<address>[0-9A-F]+)\Z')
+adsRel = re.compile (r'^\$(?P<address>[0-9A-F]+)\(PC\)\Z')
+adsInd = re.compile (r'^\(d(?P<register>[0-7]+)\)\Z')
+adsIndOff = re.compile(r'^\$(?P<address>[0-9A-F]+)\((?P<register>d[0-7])\)\Z')
+adsIndInc = re.compile (r'^\(d(?P<register>[0-7]+)\)\+\Z')
+adsIndDec = re.compile(r'^-\(d(?P<register>[0-7]+)\)\Z')
+
+#Find labels & corresponding addresses
 def labelParse(arg):
     arg = arg.strip()
     #print(arg)
     if arg[0] == '.':
         return 1
-    elif arg[0] != '#':
+    elif arg[0] != '#' and arg[0] != '':
         instParse(arg)
 
+#Parse instructions
 def instParse(arg):
     arg = arg.replace(',', ' ')
     arg = arg.split()
@@ -34,14 +45,29 @@ def instParse(arg):
     if arg[0].upper() == 'MOV':
         opcode = "01"
         byteCount = byteCount+1
+        m = modeParse(arg[1])
+        print(m[0])
         
-        #if arg[2].upper()[0] == 'D':
-         #   opcode = opcode + "0"
-        if arg[2].upper()[0] == '$' and 1==0:
-            print("lol")
+#Parse addressing modes
+def modeParse(arg):
+    if adsReg.match(arg):
+        return [0,2]
+    elif adsDir.match(arg):
+        return [1,2]
+    elif  adsRel.match(arg):
+        return [2,2]
+    elif adsInd.match(arg):
+        return [3,2]
+    elif adsIndOff.match(arg):
+        return [4,2]
+    elif adsIndInc.match(arg):
+        return [5,2]
+    elif adsIndDec.match(arg):
+        return [6,2]
+    elif adsImm.match(arg):
+        return [7,2]
         
-    
-
+#First pass    
 with open("input.asm") as f:
     for line in f:
         print(labelParse(line))
--- Assembler/input.asm
+++ Assembler/input.asm
@@ -3,3 +3,6 @@
 	MOV #2,d0
 	MOV $123,d2
 	MOV $95(PC),d5
+	MOV (d4),d2
+	MOV $4F(d3),d7
+	MOV $3,$2
